9117c61ecb4cba6499982e7a4f77f3107cbacee2
unknown <RacerSupra90@gmail.com>
2024-11-14 12:36:56 +0200

    bike_6_2

---
+++ data.js
@@ -0,0 +1,21 @@
+var data = [
+  {
+    title: "Карамелька",
+    nick: "karamelka",
+    avatar: "/images/karamelka.jpeg",
+    desc: "Cамый младший член семьи. Коронная фраза — «Я знаю, что.",
+  },
+  {
+    title: "Компот",
+    nick: "kompot",
+    avatar: "/images/kompont.png",
+    desc: "Cтарший котёнок в семье. Ходит в школу. Очень любит",
+  },
+  {
+    title: "Коржик",
+    nick: "korzhik",
+    avatar: "/images/korzhik.png",
+    desc: "Средний котёнок в семье. Очень любит футбол. Любит",
+  },
+];
+module.exports.data = data;
