d00495ce641c99547eafbf990ccb655ccd93ff69
unknown <RacerSupra90@gmail.com>
2024-11-21 14:44:56 +0200

    bike_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/testMongoose2024");
-const Bike = mongoose.model("bike", { name: String });
-const bike = new bike({ name: "Enduero" });
-bike.save().then(() => console.log("lll"));
+var schema = mongoose.Schema({ name: String });
+schema.methods.meow = function () {
+  console.log(this.name + "едит по горам");
+};
+const Bike = mongoose.model("Bike", schema);
+const bikey = new Bike({ name: "Велосипкд" });
+bikey.save().then(() => bikey.meow());
