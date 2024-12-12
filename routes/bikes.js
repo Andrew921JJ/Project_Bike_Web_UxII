08d3dad937006a68fb384c4e4557eba004854ece
unknown <RacerSupra90@gmail.com>
2024-12-12 12:18:20 +0200

    bike_8_3

--- routes/bikes.js
+++ routes/bikes.js
@@ -1,14 +1,28 @@
 var express = require("express");
 var router = express.Router();
-
+var Bike = require("../models/bike").Bike;
 // /* GET users listing. */
 // router.get("/", function (req, res, next) {
 //   res.send("Новый маршрутизатор, для маршрутов, начинающихся с bikes");
 // });
 
+// /* Страница */
+// router.get("/:nick", function (req, res, next) {
+//   res.send(req.params.nick);
+// });
+
 /* Страница котов */
-router.get("/:nick", function (req, res, next) {
-  res.send(req.params.nick);
+router.get("/:nick", async function (req, res, next) {
+  console.log("Z pltcm");
+  var bikes = await Bike.find({ nick: req.params.nick });
+  console.log(bikes);
+  if (!bikes.length) return next(new Error("/////"));
+  var bikes = bikes[0];
+  res.render("bike", {
+    title: bikes.title,
+    picture: bikes.avatar,
+    desc: bikes.desc,
+  });
 });
 
 module.exports = router;
