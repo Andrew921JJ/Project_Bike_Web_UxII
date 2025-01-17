d6bf0297a3632acfeb8233ca4807946abeabde23
unknown <RacerSupra90@gmail.com>
2025-01-17 17:21:38 +0200

    bike_9_2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.cookie("greeting", "Hi!!!").render("index", { title: "Express" });
 });
 
 router.get("/Dirt", function (req, res, next) {
